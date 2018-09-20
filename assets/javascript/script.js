b79e1bc0ea3328b33ce5837c3979ddd24b014187
Sami B <sboleyn@email.arizona.edu>
2018-09-20 15:38:51 -0700

    update checklist at bottom of page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -198,8 +198,3 @@ $("body").on("click", ".gif", function () {
 Allow users to request additional gifs to be added to the page.
 
 Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
-
-Allow users to add their favorite gifs to a favorites section.
-
-This should persist even when they select or add a new topic.
-If you are looking for a major challenge, look into making this section persist even when the page is reloaded(via localStorage or cookies). */
